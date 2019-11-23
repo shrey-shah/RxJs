2ea2d1d2db37bc0242b1f666ef6c37fd60486353
Shrey Shah <shreyshah111@gmail.com>
2019-11-23 16:57:58 +0530

    MergeMap and ExhaustMap operator

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -50,15 +50,22 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
         this.form.valueChanges
         .pipe(
             filter(() => this.form.valid),
-            concatMap(changes => this.saveCourse(changes)) // subscribe to th observable given and concats it. 
+            concatMap(changes => this.saveCourse(changes)) // subscribe to th observables given (sequentially) and concats it. 
                 //Also useful in this case since without this, there will be simultaneous requests to server for auto save. 
-                // concatmap completes first observable, then only goes for second one.
+                // concatmap completes first observable, then only goes for second one (sequential).
+                // mergemap is similar to conactmap but it subscribes to observables in parallel manner rather than sequential
         )
         .subscribe();
     }
 
     ngAfterViewInit() {
-
+        fromEvent(this.saveButton.nativeElement, 'click')
+        .pipe(
+            exhaustMap(() => this.saveCourse(this.form.value))
+        )
+        .subscribe();
+        // exhaustmap ignores other observables if one is going on.
+        //so, when POST request is in progress, all other clicks will be ignored.
     }
 
     saveCourse(changes) {
