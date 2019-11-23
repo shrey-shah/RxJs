cfdf385a784307d7a76d58531dd8a67929605918
Shrey Shah <shreyshah111@gmail.com>
2019-11-23 17:38:12 +0530

    search filter and SwitchMap operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { UtilService } from '../common/util';
 
 
 @Component({
@@ -24,28 +25,37 @@ import {Lesson} from '../model/lesson';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-
-
+    course$: Observable<Course>;
+    lessons$: Observable<Lesson[]>;
+    courseId: string;
     @ViewChild('searchInput', { static: true }) input: ElementRef;
 
-    constructor(private route: ActivatedRoute) {
-
-
+    constructor(private route: ActivatedRoute,private _util: UtilService) {
     }
 
     ngOnInit() {
 
-        const courseId = this.route.snapshot.params['id'];
-
-
-
+        this.courseId = this.route.snapshot.params['id'];
+        this.course$ = this._util.createHttpObservable(`/api/courses/${this.courseId}`);
+        this.lessons$ = this.loadLessons();
     }
 
     ngAfterViewInit() {
+        fromEvent<any>(this.input.nativeElement, 'keyup')
+        .pipe(
+            map(event => event.target.value),
+            debounceTime(400),
+            distinctUntilChanged(),
+            switchMap(search => this.loadLessons(search)) // cancels the current observable if any new observable arrives. best suitable in search filtering
+        )
+        .subscribe(console.log);
+    }
 
-
-
-
+    loadLessons(filter = '') {
+        return this._util.createHttpObservable(`/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${filter}`)
+        .pipe(
+            map(res => res["payload"])
+        );
     }
 
 
